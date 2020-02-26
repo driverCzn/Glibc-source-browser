9b8db2df68b937f8de655870b082b901a5c060df
Just1earnm0re <driverczn@gmail.com>
2020-02-26 14:47:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,13 +18,17 @@ SOURCE_DIRECTORY=~/glibc/glibc_src
 BUILD_DIRECTORY=~/glibc/glibc_src_"$VERSION"_build
 
 # run this script under woboq_codebrowser/
+if [ -x ./generator/codebrowser_generator ] && [ -x ./indexgenerator/codebrowser_indexgenerator ]; then
+    ./generator/codebrowser_generator -b $BUILD_DIRECTORY -a -o $OUTPUT_DIRECTORY -p $PROJ_NAME:$SOURCE_DIRECTORY:$VERSION
+    ./indexgenerator/codebrowser_indexgenerator $OUTPUT_DIRECTORY
+    #cp -rv ./data $DATA_DIRECTORY
 
-./generator/codebrowser_generator -b $BUILD_DIRECTORY -a -o $OUTPUT_DIRECTORY -p $PROJ_NAME:$SOURCE_DIRECTORY:$VERSION
-./indexgenerator/codebrowser_indexgenerator $OUTPUT_DIRECTORY
-cp -rv ./data $DATA_DIRECTORY
-
-echo -e "\n"
-echo "Finish!"
-echo "Now you can open file://$OUTPUT_DIRECTORY/index.html for a quick look."
-echo "Host the directory with web server(e.g. nginx) to get the most out of it!"
-echo -e "\n"
+    echo -e "\n"
+    echo "Finish!"
+    echo "Now you can open file://$OUTPUT_DIRECTORY/index.html for a quick look."
+    echo "Host the directory with web server(e.g. nginx) to get the most out of it!"
+    echo -e "\n"
+else
+    echo "Compile the generator first"
+    echo "see https://github.com/woboq/woboq_codebrowser#compiling-the-generator-on-linux"
+fi
